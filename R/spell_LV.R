96ca3a82abab27661199c2f0da3095412da0b4d3
DHintz137 <dhintz1@uwyo.edu>
2022-10-20 21:29:52 -0600

    changing import to importFrom for LV

--- R/spell_LV.R
+++ R/spell_LV.R
@@ -13,7 +13,7 @@ library(stringdist)
 #' @param threshold a number to set level of sensitivity 
 #'
 #' @author Daniel Hintz
-#' @import stringdist
+#' @importFrom stringdist stringdist
 #' @return a character vector
 #' @export
 #'
