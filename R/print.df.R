491e858cfc3b09585ad837e69e414def461ccee3
DHintz137 <dhintz1@uwyo.edu>
2022-11-16 15:00:14 -0700

    break my main

---
+++ R/print.df.R
@@ -0,0 +1,42 @@
+#' Title
+#'
+#' @param x 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+print.df  <- function(x,...) {
+  # small dimensions (cond.1)
+  if(dim(as.matrix(x))[2] ==1 & dim(as.matrix(x))[1] <= 38){
+    knitr::kable(
+      x, align = "c",
+      digits = 3,
+      format="latex",
+      booktabs = T,
+      caption = ""
+    ) %>% # styling
+      kable_styling(latex_options = c("HOLD_position"),
+                    font_size = 9.5,full_width = F) %>% 
+      asis_output()
+  } else{
+    # dimensions to large (cond.2)
+    if(ncol(x)>= 10 | nrow(x) > 38){
+      x %>% as_tibble() %>%  capture.output() %>% cat(sep = "\n")
+      cat("WARNING: output out off bounds, defualting to tibble. See print.df")
+    } else{
+      # dimensions fit asis (cond.3)
+      knitr::kable(
+        x, 
+        align = "c",
+        digits = 3,
+        format="latex",
+        booktabs = T,
+        caption = ""
+      ) %>% # styling
+        kable_styling(latex_options = c("HOLD_position"),
+                      font_size = 9.5,full_width = F) %>% 
+        asis_output()
+    }
+  }}
