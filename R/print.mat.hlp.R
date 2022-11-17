8fd76d490a215ea8920dae3953b720d432c7a166
DHintz137 <dhintz1@uwyo.edu>
2022-11-16 21:06:32 -0700

    documented print.mat.hlp

---
+++ R/print.mat.hlp.R
@@ -0,0 +1,30 @@
+#' Helper function for print.mat
+#'
+#' @param x a vector
+#' @param ... an ellipse 
+#'
+#' @return a vector
+#' @export
+
+print.mat.hlp <- function(x, ...) {
+  # less than 3 values (cond.1)
+  if(length(as.vector(as.matrix(x))) < 3){
+    cat("cond.1: nobs. < 3, see print.mat")
+  } else{
+    # small dimensions (cond.2)
+    # ncol                            #nrow
+    if(dim(as.matrix(x))[2] ==1 & dim(as.matrix(x))[1] <= 38){
+      cat("cond.2: nrow <= 38 AND ncol ==1, see print.mat")
+    } else{
+      #dimensions to large (cond.3)
+      if(dim(as.matrix(x))[2] >= 10 | dim(as.matrix(x))[1] > 38){
+        cat("cond.3: nrow > 38 OR ncol >= 10, see print.mat")
+      } else{
+        #dimensions fit asis (cond.4)
+        cat("cond.4: dim none of following", "\n", 
+            "- nobs. < 3", "\n", 
+            "- nrow <= 38 AND ncol ==1", "\n",
+            "- nrow > 38 OR ncol >= 10", "\n",
+            "see print.mat")
+      }}}
+}
