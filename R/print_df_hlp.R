f24620a008664273b08ff436643f6ebe9ffcc8d6
DHintz137 <dhintz1@uwyo.edu>
2022-11-19 21:39:03 -0700

    replacing last dots for _

--- R/print_df_hlp.R
+++ R/print_df_hlp.R
@@ -10,16 +10,16 @@
 print_df_hlp  <- function(x,...) {
   # small dimensions (cond.1)
   if(dim(as.matrix(x))[2] ==1 & dim(as.matrix(x))[1] <= 38){
-    cat("cond.1: nrow <= 38 AND ncol == 1, see print.df")
+    cat("cond.1: nrow <= 38 AND ncol == 1, see print_df")
   } else{
     # dimensions to large (cond.2)
     if(ncol(x)>= 10 | nrow(x) > 38){
-      cat("cond.2: nrow > 38 OR ncol >= 10, see print.df")
+      cat("cond.2: nrow > 38 OR ncol >= 10, see print_df")
     } else{
       # dimensions fit asis (cond.3)
       cat("cond.3: dim none of following:", "\n", 
           "- nrow <= 38 AND ncol == 1", "\n", 
           "- nrow > 38 OR ncol >= 10", "\n",
-          "see print.df")
+          "see print_df")
     }
   }}
