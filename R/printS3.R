a5a1e483c630b3ed446174343f1b6e97e547b68a
DHintz137 <dhintz1@uwyo.edu>
2022-11-19 22:15:55 -0700

    update printS3

--- R/printS3.R
+++ R/printS3.R
@@ -5,6 +5,6 @@
 #' @export
 
 printS3 <- function(){
-  registerS3method("knit_print", "data.frame", print.df)
-  registerS3method("knit_print", "numeric", print.mat)
+  registerS3method("knit_print", "data.frame", print_df)
+  registerS3method("knit_print", "numeric", print_vec)
 }
